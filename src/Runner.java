4db5490397078d74a7d307ebaf8a5d249c18bb14
Pammy Hunter <pjhunter@dmacc.com>
2019-04-18 19:29:24 -0500

    second commit

---
+++ src/Runner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Runner{
+    public static void main( String[] args ) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("userName");
+        String userName = in.next();
+        System.out.println("number");
+        Integer inputNumber = in.nextInt();
+    }
+}
