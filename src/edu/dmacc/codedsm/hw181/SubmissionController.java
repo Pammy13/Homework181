93cba85a4407076cc3e8759f923291ac4e0207ab
Pammy Hunter <pjhunter@dmacc.com>
2019-04-18 19:23:54 -0500

    first commit

---
+++ src/edu/dmacc/codedsm/hw181/SubmissionController.java
@@ -0,0 +1,9 @@
+package edu.dmacc.codedsm.hw181;
+
+public interface SubmissionController{
+     void submit( Integer inputNumber, String userName);
+
+
+
+
+}
---
+++ src/edu/dmacc/codedsm/hw181/SubmissionControllerImpl.java
@@ -0,0 +1,9 @@
+package edu.dmacc.codedsm.hw181;
+
+public class SubmissionControllerImpl implements SubmissionController{
+    @Override
+    public void submit( Integer inputNumber, String userName ) {
+
+    }
+
+}
